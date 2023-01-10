fea618fe61a41cf551b5e2fea7a2881f52de3be7
ramatama42 <ramadhantama42@gmail.com>
2023-01-10 13:48:41 +0700

    Update

---
+++ app/src/main/java/com/ramadhan/drugstore/ModelApotik.java
@@ -0,0 +1,38 @@
+package com.ramadhan.drugstore;
+
+public class ModelApotik {
+    private String nama, tentang, foto, kordinat;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getTentang() {
+        return tentang;
+    }
+
+    public void setTentang(String tentang) {
+        this.tentang = tentang;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getKordinat() {
+        return kordinat;
+    }
+
+    public void setKordinat(String kordinat) {
+        this.kordinat = kordinat;
+    }
+}
+}
